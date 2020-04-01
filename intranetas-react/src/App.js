7d90a163a085d160f5d8c29222077437169ed49d
Urtė Bankauskaitė <urtban@ktu.lt>
2020-04-01 19:42:31 +0300

    'added'

--- intranetas-react/src/App.js
+++ intranetas-react/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { LogIn } from './login';
+import { SideBar } from './SideNav';
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path={'/login'} component={LogIn} />
-                <Redirect exact from={'/'} to={'/login'} />
+                <Route path={'/login'} component={LogIn}/>  
+                <Route path={'/SideNav'} component={SideBar} />
+                <Redirect exact from={'/'} to={'/SideNav'} />
             </Switch>
         </BrowserRouter>
     );
---
+++ intranetas-react/src/SideNav/index.js
@@ -0,0 +1,5 @@
+﻿import SideBar from './SideBar'
+
+export {
+    SideBar
+}
