f0a4efc3cdea4aaf43cedd912912badb75ad0bdc
Karolis Vaitelė <karvai11@ktu.lt>
2020-04-14 01:51:41 +0300

    Add login design

--- intranetas-react/src/App.js
+++ intranetas-react/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { LogIn } from './login';
 import { HomePage } from './homepage';
+import { Helmet } from 'react-helmet';
 
 function App() {
     return (
         <BrowserRouter>
+            <Helmet>
+                <title>Dewbridge</title>
+            </Helmet>
             <Switch>
                 <Route path={'/login'} component={LogIn}/>  
                 <Route path={'/home'} component={HomePage} />
--- intranetas-react/src/login/LogIn.jsx
+++ intranetas-react/src/login/LogIn.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from 'react'
-import { Button, FormGroup, FormControl } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Redirect } from 'react-router-dom';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            // light: will be calculated from palette.primary.main,
+            main: '#ab0909',
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+        secondary: {
+            light: '#0066ff',
+            main: '#770606',
+            // dark: will be calculated from palette.secondary.main,
+            contrastText: '#ffcc00',
+        },
+    },
+});
+
 
 export default () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loggedIn, setLoggedIn] = useState(false);
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
@@ -12,33 +37,91 @@ export default () => {
 
     function handleSubmit(event) {
         event.preventDefault();
+        if (validateForm) {
+            setLoggedIn(true);
+        }
     }
+
+    function handleChange(event) {
+        event.preventDefault();
+        if (event.target.name === 'email') {
+            setEmail(event.target.value);
+        }
+        if (event.target.name === 'password') {
+            setPassword(event.target.value);
+        }
+    }
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            marginTop: theme.spacing(8),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing(1),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+    }));
+    const classes = useStyles();
+
     return (
-        <div className="login">
-            <form onSubmit={handleSubmit} className="login-form">
-                <FormGroup controlId="email" bsSize="large">
-                    <label>Email</label>
-                    <FormControl
+        loggedIn ? (
+            <Redirect to="/home" />
+        ) : 
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit} noValidate>
+                    <ThemeProvider theme={theme}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
                         autoFocus
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
+                        onChange={handleChange}
                     />
-                </FormGroup>
-                <FormGroup controlId="password" bsSize="large">
-                    <label>Password</label>
-                    <FormControl
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
                         type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={handleChange}
                     />
-                </FormGroup>
-                <Link to="/home">
-                    <Button block bsSize="large" disabled={!validateForm()} type="submit">
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
                             Login
                     </Button>
-                </Link>
-            </form>
-        </div>
+                    </ThemeProvider>
+                </form>
+            </div>
+            <Box mt={8}>
+            </Box>
+        </Container>
         )
 }
