a80457cefdfaaca7a3e3c3b7585c0d996f5d2b67
Karolis Vaitelė <karvai11@ktu.lt>
2020-04-14 03:01:25 +0300

    Add calendar to header, dropdown style

--- intranetas-react/src/Components/Header.jsx
+++ intranetas-react/src/Components/Header.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/components/header.css';
+import Calendar from 'react-calendar'
 
 
 export default () => {
+    const [displayCalendar, setDisplayCalendar] = useState(false);
+
+    function handleClick() {
+        setDisplayCalendar(displayCalendar ? false : true);
+        if (!displayCalendar) {
+            document.getElementById("header-calendar").style.height = "272px";
+        }
+        else {
+            document.getElementById("header-calendar").style.height = "0";
+        }
+    } 
     return (
-        <div>
+        <div className="header-wrapper">
             <div class="header">
                 <a href="/home" class="logo">dewbridge</a>
                 <div class="header-right">
                     <a href="/home">Home</a>
+                    <a onClick={handleClick}>Calendar</a>
                     <a href="#contact">Profile</a>
                 </div>
+            </div>
+            <div id="header-calendar">
+                <Calendar className='header-calendar' />
             </div>
         </div>
         )
--- intranetas-react/src/homepage/HomePage.jsx
+++ intranetas-react/src/homepage/HomePage.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SideBar } from '../Components'
 import { Header } from '../Components'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 export default () => {
+
     return (
         <div className="homePageWrapper">
             <Header />
