0287be78c2fcc35041d7785124a874752ba5c67d
Urtė Bankauskaitė <urtban@ktu.lt>
2020-04-27 00:46:45 +0300

    Start of Front end of Career page

--- web-react/src/Career/Career.jsx
+++ web-react/src/Career/Career.jsx
@@ -4,8 +4,45 @@ import { Header } from '../_components'
 import { Footer } from '../_components'
 import { userActions } from '../_actions';
 import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
 import '../css/main.css';
 import '../css/career.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import { spacing } from '@material-ui/system';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+function createData(name, pay, hours, needed) {
+    return { name, pay, hours, needed };
+}
+
+function createData1(position, status) {
+    return { position, status };
+}
+
+const rows = [
+    createData('Software Engineer', 1590, 6.0, 'IT bachelor`s degree'),
+    createData('Profesional procrastinator', 15900, 8.0, 'Being a procrastinator certificate'),
+    createData('Lel', 100, 1.0, 'X)'),
+    createData('Software Engineer', 1590, 6.0, 'IT bachelor`s degree'),
+    createData('Software Engineer', 1590, 6.0, 'IT bachelor`s degree'),
+];
+
+const rows1 = [
+    createData1('Software Engineer', 'Ne'),
+    createData1('Profesional procrastinator', 'Accepted'),
+];
 
 class Career extends React.Component {
     render() {
@@ -15,6 +52,65 @@ class Career extends React.Component {
                 <SideBar />
                 <div className="content">
                     <div className="heading1"> Current position </div>
+                    <Grid item xs={12} sm={4}>
+                        <div className="pap">
+                            <div className="heading2"> Piece of shit</div>
+                            <Grid container direction="column" justify="left" alignItems="left">
+                                <div className="text"> Current pay: 0 </div>
+                                <div className="text"> Time: all </div>
+                            </Grid>
+                        </div>
+                    </Grid>
+                    <div className="heading1"> Available positions </div>
+                    <Grid item xs={12} sm={8} pb={20}>
+                        <TableContainer component={Paper} >
+                            <Table className={useStyles.table} aria-label="simple table">
+                            <TableHead className="TableHead">
+                                    <TableRow>
+                                        <TableCell > <div className="headCell">Position</div></TableCell>
+                                        <TableCell align="right" > <div className="headCell">Pay</div></TableCell>
+                                        <TableCell align="right" ><div className="headCell">Hours / day</div></TableCell>
+                                        <TableCell align="right" ><div className="headCell">Required things</div></TableCell>                                    
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow key={row.name}>
+                                        <TableCell component="th" scope="row">
+                                            <div className="tableCell">{row.name}</div>
+                                        </TableCell>
+                                        <TableCell align="right"><div className="tableCell">{row.pay}</div></TableCell>
+                                        <TableCell align="right"><div className="tableCell">{row.hours}</div></TableCell>
+                                        <TableCell align="right"><div className="tableCell">{row.needed}</div></TableCell>                                        
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        </TableContainer>
+                    </Grid>
+                    <div className="heading1"> My applications </div>
+                    <Grid item xs={12} sm={6} pb={20}>
+                        <TableContainer component={Paper}>
+                            <Table className={useStyles.table} aria-label="simple table">
+                                <TableHead className="TableHead">
+                                    <TableRow>
+                                        <TableCell > <div className="headCell">Position</div></TableCell>
+                                        <TableCell align="right" > <div className="headCell">Status</div></TableCell>                                        
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rows1.map((row) => (
+                                        <TableRow key={row.position}>
+                                            <TableCell component="th" scope="row">
+                                                <div className="tableCell">{row.position}</div>
+                                            </TableCell>
+                                            <TableCell align="right"><div className="tableCell">{row.status}</div></TableCell>                                            
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
                 </div>
                 <Footer />
             </div>
